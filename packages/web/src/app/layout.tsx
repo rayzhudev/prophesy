5d8ad5910639be783f27b72b12d691ce0422a1d6
Ray Zhu <rayzhudev@gmail.com>
2024-12-30 20:39:14 +1100

    set favicon

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Prophesy",
   description: "A text spacing app",
+  icons: {
+    icon: [{ url: "/sparks.svg", type: "image/svg+xml" }],
+  },
 };
 
 export default function RootLayout({
